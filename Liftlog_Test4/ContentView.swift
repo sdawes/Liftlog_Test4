434391c2df190733eec028dbede2c0192aac5d4e
Stephen Dawes <stephenrdawes@gmail.com>
2025-02-15 12:12:23 +0000

    Initial Commit

---
+++ Liftlog_Test4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Liftlog_Test4
+//
+//  Created by Stephen Dawes on 15/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Liftlog_Test4/Liftlog_Test4App.swift
@@ -0,0 +1,17 @@
+//
+//  Liftlog_Test4App.swift
+//  Liftlog_Test4
+//
+//  Created by Stephen Dawes on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Liftlog_Test4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
